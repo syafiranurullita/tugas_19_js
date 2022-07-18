bf4bc8c24cfa5df8bf9b7368c9dd85f814bdaf49
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:51:55 +0700

    tugas 19 js
    untuk niomic

---
+++ Tugas19/Tugas19.js
@@ -0,0 +1,17 @@
+function repeat() {
+  var isi = "Semangat...";
+  console.log(isi.repeat(5));
+
+  
+  console.log("*".repeat(5));
+  console.log("*".repeat(10));
+  console.log("*".repeat(15));
+  console.log("*".repeat(20));
+  console.log("*".repeat(25));
+  console.log("*".repeat(20));
+  console.log("*".repeat(15));
+  console.log("*".repeat(10));
+  console.log("*".repeat(5));
+}
+
+repeat();
